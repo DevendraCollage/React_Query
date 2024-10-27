49df91978d237ae0ed426705d7fc91270949732e
Devendra Parmar <23030501043@darshan.ac.in>
2024-10-27 22:07:33 +0530

    How to implement the React Query Dev Tools to test for the developer tools easily

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import MainLayout from "./Components/Layout/MainLayout";
 import Home from "./Pages/Home";
@@ -36,6 +37,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>;
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
